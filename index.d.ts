d3437a70f29644d0b428fa93fea6ef174d957d2f
Yure <yure@project-nexs.com>
2025-04-08 13:18:38 +0600

    Fixes

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,9 @@
 import parse from './lib/parse';
 import reskify from './lib/reskify';
-import { readFile, readFileAsync, load } from './lib/read';
 
 export interface Reski {
     parse: typeof parse;
     reskify: typeof reskify;
-    load: typeof load;
-    readFile: typeof readFile;
-    readFileAsync: typeof readFileAsync;
 }
 
 declare const Reski: Reski;
--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 import parse from './lib/parse.js';
 import reskify from './lib/reskify.js';
-import { readFile, readFileAsync, load } from './lib/read.js';
 
 
 const Reski = {
     parse: parse,
-    reskify: reskify,
-    load: load,
-    readFile : readFile,
-    readFileAsync : readFileAsync
+    reskify: reskify
 }
 
 export default Reski;
